802c81d5e0e57105e404a53e86b64db3cf15c03c
hossammenem <hossammenem0102840@gmail.com>
2024-10-22 16:02:43 +0300

    fixed some errors, still no understand

---
+++ Notes.md
@@ -0,0 +1,2 @@
+### Current Stage
+trying to understand how this library work, more fuackary is happening, probably gonna be documented here
--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,23 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Network.Socket
 import Network.Socket.ByteString (recv, sendAll)
+
 import qualified Data.ByteString as S
-import Control.Monad (forever)
-import Control.Exception (finally)
+import qualified Control.Exception as E
+import qualified Data.List.NonEmpty as NE
+
+import Control.Concurrent (forkFinally)
+
+import Control.Monad (forever, unless, void)
+import Control.Exception
 
 
--- what's unless?
--- do notation? and <-
--- bind the return of a monadic action to a variable
--- $ ???
--- what's () after IO
+main :: IO ()
+main = putStrLn "fuckoff"
 
-setupServer :: Int -> IO ()
-setupServer = runTcpServer Nothing 3000 talk
+setupServer :: Maybe String -> [Char] -> IO ()
+setupServer mhostN port = runTCPServer mhostN port talk
 	where
 		talk s = do
 			msg <- recv s 1024
@@ -23,13 +25,37 @@ setupServer = runTcpServer Nothing 3000 talk
 				sendAll s msg
 				talk s
 
+runTCPServer :: Maybe HostName -> ServiceName -> (Socket -> IO a) -> IO a
+runTCPServer mhost port server = withSocketsDo $ do -- this withSocketDo is for windows, cuz windows is gay
+    addr <- resolve
+    E.bracket (open addr) close loop
+		where
+			resolve = do
+				let hints = defaultHints {
+					addrFlags = [AI_PASSIVE]
+						, addrSocketType = Stream
+				}
+				NE.head <$> getAddrInfo (Just hints) mhost (Just port)
+			open addr = E.bracketOnError (openSocket addr) close $ \sock -> do
+				setSocketOption sock ReuseAddr 1
+				withFdSocket sock setCloseOnExecIfNeeded
+				bind sock $ addrAddress addr
+				listen sock 1024
+				return sock
+			loop sock = forever $ E.bracketOnError (accept sock) (close . fst) $ \(conn, _peer) -> void $
+				-- 'forkFinally' alone is unlikely to fail thus leaking @conn@,
+				-- but 'E.bracketOnError' above will be necessary if some
+				-- non-atomic setups (e.g. spawning a subprocess to handle
+				-- @conn@) before proper cleanup of @conn@ is your case
+				forkFinally (server conn) (const $ gracefulClose conn 5000)
+
 
 connectToClient :: Int -> IO ()
-connectToClient = Error "not implemented"
+connectToClient _ = error "not implemented"
 
-handleClient :: Socket -> IO ()
-handleClient conn = do
-    msg <- recv conn 1024
-    putStrLn $ "Received: " ++ show msg
-
-    sendAll conn "Hello, Client!"
+-- handleClient :: Socket -> IO ()
+-- handleClient conn = do
+--     msg <- recv conn 1024
+--     putStrLn $ "Received: " ++ show msg
+-- 
+--     sendAll conn "Hello, Client!"
